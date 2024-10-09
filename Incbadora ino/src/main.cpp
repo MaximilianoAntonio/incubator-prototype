fc51cf266e81ea98c020f11ff534d4e027d7c5d8
Maximiliano <maximilianogaetepizza@gmail.com>
2024-10-09 20:56:45 -0300

    .

--- Incbadora ino/src/main.cpp
+++ Incbadora ino/src/main.cpp
@@ -2,27 +2,25 @@
 #include <PID_v1.h>
 
 // Definición de pines
-#define NTC_PIN A0 // pin NTC
-#define LUZ_PIN 11  // pin Luz
-#define VENT_PIN 9 // pin Ventilador
-#define VEL_PIN 2  // pin Velocidad Tacometro
+#define NTC_PIN A0 // Pin NTC
+#define LUZ_PIN 11 // Pin Luz
+#define VENT_PIN 9 // Pin Ventilador
+#define VEL_PIN 2 // Pin Velocidad Tacómetro
 
 // Constantes para el cálculo de la temperatura
 #define A_COEFF 0.5458630405e-3
 #define B_COEFF 2.439180157e-4
 #define C_COEFF -0.0003705076153e-7
 
-// Variables para medición de temperatura y control
+// Variables para mediciones y control
 float resistenciaNTC, logResistencia, tempKelvin, tempCelsius;
-int lecturaNTC, potenciaVentilador, potenciaLuz, velocidadVentilador;
-volatile int contadorImpulsos = 0; // Declarar como volatile
-volatile float velocidadRPM = 0;   // Declarar como volatile
-unsigned long tiempoAnteriorTemp, tiempoAnteriorVel, tiempoImprimir;
-unsigned long ultimoTiempoRPM;
-
+int lecturaNTC, potenciaVentilador, potenciaLuz;
+volatile int contadorImpulsos = 0;
+volatile float velocidadRPM = 0;
+unsigned long tiempoAnteriorTemp, tiempoAnteriorVel, tiempoImprimir, ultimoTiempoRPM;
 
 // Variables para lectura de comandos
-String comando, valorLuz, valorVentilador;
+String comando;
 
 // Variables PID
 double Setpoint, Input, Output;
@@ -32,38 +30,25 @@ PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 // Variables para control automático
 bool control_automatico = false;
 
-// Definir un arreglo para almacenar las últimas 5 lecturas de RPM
-float ultimasRPM[5] = {0, 0, 0, 0, 0}; // Inicialmente con ceros
-int indiceRPM = 0;  // Índice para llevar la cuenta de la posición actual
-float promedioRPM ; // Variable para almacenar el promedio de las últimas 5 lecturas de RPM
+// Arreglos para almacenar últimas 5 lecturas
+float ultimasRPM[5] = {0};
+float ultimasTemp[5] = {0};
+int indiceRPM = 0;
+int indiceTemp = 0;
 
-// Definir un arreglo para almacenar las últimas 5 lecturas de temperatura
-float ultimasTemp[5] = {0, 0, 0, 0, 0}; // Inicialmente con ceros
-int indiceTemp = 0;  // Índice para llevar la cuenta de la posición actual
-
-// Función para calcular el promedio de los últimos 5 valores de RPM
-float calcularPromedioRPM() {
+// Funciones para calcular promedios
+float calcularPromedio(float *valores, int tamano) {
   float suma = 0;
-  for (int i = 0; i < 5; i++) {
-    suma += ultimasRPM[i];
+  for (int i = 0; i < tamano; i++) {
+    suma += valores[i];
   }
-  return suma / 5;  // Devolver el promedio
-}
-
-// Función para calcular el promedio de los últimos 5 valores de temperatura
-float calcularPromedioTemp() {
-  float suma = 0;
-  for (int i = 0; i < 5; i++) {
-    suma += ultimasTemp[i];
-  }
-  return suma / 5;  // Devolver el promedio
+  return suma / tamano;
 }
 
 // Función de interrupción para contar los impulsos del ventilador
 void medirVelocidad() {
-    contadorImpulsos++;
+  contadorImpulsos++;
 }
-
 
 void setup() {
   Serial.begin(115200);
@@ -83,113 +68,82 @@ void setup() {
   // Configuración de interrupción para medir velocidad del ventilador
   attachInterrupt(digitalPinToInterrupt(VEL_PIN), medirVelocidad, FALLING);
   myPID.SetMode(AUTOMATIC);
-  myPID.SetOutputLimits(0, 100); 
+  myPID.SetOutputLimits(0, 100);
 }
 
 void loop() {
-
   // Medición de temperatura cada 250 ms
   if (millis() - tiempoAnteriorTemp >= 250) {
-    // Lectura del sensor de temperatura
     lecturaNTC = analogRead(NTC_PIN);
-    resistenciaNTC = 100000.0 * ((1023.0 / lecturaNTC) - 1.0); // Cálculo de la resistencia
-    logResistencia = log(resistenciaNTC); // Logaritmo natural de la resistencia
-    tempKelvin = 1.0 / (A_COEFF + B_COEFF * logResistencia + C_COEFF * pow(logResistencia, 3)); // Conversión a grados Kelvin
-    tempCelsius = tempKelvin - 273.15; // Conversión a grados Celsius
+    resistenciaNTC = 100000.0 * ((1023.0 / lecturaNTC) - 1.0);
+    logResistencia = log(resistenciaNTC);
+    tempKelvin = 1.0 / (A_COEFF + B_COEFF * logResistencia + C_COEFF * pow(logResistencia, 3));
+    tempCelsius = tempKelvin - 273.15;
 
-    // Actualizar el arreglo con la nueva lectura de temperatura
     ultimasTemp[indiceTemp] = tempCelsius;
-    indiceTemp = (indiceTemp + 1) % 5;  // Mover el índice circularmente entre 0 y 4
+    indiceTemp = (indiceTemp + 1) % 5;
 
-    // Si el control automático está activado, actualizamos el PID
     if (control_automatico) {
-      Input = calcularPromedioTemp();
+      Input = calcularPromedio(ultimasTemp, 5);
       myPID.Compute();
       analogWrite(LUZ_PIN, map(Output, 0, 100, 0, 255));
     }
 
-    tiempoAnteriorTemp = millis(); // Actualizar el tiempo de la última medición de temperatura
+    tiempoAnteriorTemp = millis();
   }
 
   // Cálculo de la velocidad del ventilador cada 1000 ms
-
   if (millis() - tiempoAnteriorVel >= 1000) {
     unsigned long tiempoActual = millis();
     unsigned long tiempoTranscurrido = tiempoActual - ultimoTiempoRPM;
 
-    // Deshabilitar interrupciones mientras se lee y reinicia el contador
     noInterrupts();
     int pulsos = contadorImpulsos;
-    contadorImpulsos = 0; // Reiniciar el contador de pulsos
+    contadorImpulsos = 0;
     interrupts();
 
-    if (tiempoTranscurrido > 0 && pulsos > 0) {
-      // Suponiendo que el ventilador genera 2 pulsos por revolución
+    if (tiempoTranscurrido > 0) {
       velocidadRPM = (pulsos) * (6000.0 / tiempoTranscurrido);
-    } else if (tiempoTranscurrido > 0 && pulsos == 0) {
-      velocidadRPM = 0;  // Si no hay pulsos, la velocidad es 0
     }
 
-    // Actualizar el arreglo con la nueva lectura de RPM
     ultimasRPM[indiceRPM] = velocidadRPM;
-    indiceRPM = (indiceRPM + 1) % 5;  // Mover el índice circularmente entre 0 y 4
+    indiceRPM = (indiceRPM + 1) % 5;
 
-    // Calcular el promedio de las últimas 5 lecturas de RPM
-    promedioRPM = calcularPromedioRPM();
-
-    ultimoTiempoRPM = tiempoActual;  // Actualizar el tiempo de referencia
-    tiempoAnteriorVel = tiempoActual; // Actualizar el tiempo de la última medición de velocidad
+    ultimoTiempoRPM = tiempoActual;
+    tiempoAnteriorVel = tiempoActual;
   }
 
   // Impresión de datos cada 300 ms
   if (millis() - tiempoImprimir >= 300) {
-    Serial.print(calcularPromedioTemp()); // Imprimir el promedio de temperatura
+    Serial.print(calcularPromedio(ultimasTemp, 5));
     Serial.print(";");
-    Serial.print(promedioRPM);  
+    Serial.print(calcularPromedio(ultimasRPM, 5));
     Serial.print(";");
-    Serial.println(Output); 
+    Serial.println(Output);
 
-    tiempoImprimir = millis(); // Actualizar el tiempo de la última impresión
+    tiempoImprimir = millis();
   }
 
+  // Lectura de comandos desde Serial
   if (Serial.available() > 0) {
     comando = Serial.readStringUntil('\n');
 
-    // Activar o desactivar el control automático
     if (comando == "AUTOMATIC_ON") {
-      control_automatico = true;  // Activar el control automático
-    }
-    if (comando == "AUTOMATIC_OFF") {
-      control_automatico = false;  // Desactivar el control automático (modo manual)
-    }
-
-    // Comando para ajustar el setpoint y activar el control automático
-    if (comando.startsWith("SETPOINT") && control_automatico) {
-      String valorSetpoint = comando.substring(9);  // Extraer el valor del setpoint
-      Setpoint = valorSetpoint.toDouble();  // Actualizar el setpoint del PID
-    }
-
-    // Comando para controlar la luz
-    else if (comando.startsWith("LUZ") && !control_automatico) {
-      valorLuz = comando.substring(4);
-      potenciaLuz = valorLuz.toInt();
-      potenciaLuz = map(potenciaLuz, 0, 100, 0, 255);
+      control_automatico = true;
+    } else if (comando == "AUTOMATIC_OFF") {
+      control_automatico = false;
+    } else if (comando.startsWith("SETPOINT") && control_automatico) {
+      Setpoint = comando.substring(9).toDouble();
+    } else if (comando.startsWith("LUZ") && !control_automatico) {
+      potenciaLuz = map(comando.substring(4).toInt(), 0, 100, 0, 255);
       analogWrite(LUZ_PIN, potenciaLuz);
-    }
-
-    // Comando para controlar el ventilador
-    else if (comando.startsWith("VENT") && !control_automatico) {
-      valorVentilador = comando.substring(5);
-      velocidadVentilador = valorVentilador.toInt();
-      velocidadVentilador = map(velocidadVentilador, 0, 100, 0, 255);
-      // Corriente de arranque
-      if (velocidadVentilador > 0) {
+    } else if (comando.startsWith("VENT") && !control_automatico) {
+      potenciaVentilador = map(comando.substring(5).toInt(), 0, 100, 0, 255);
+      if (potenciaVentilador > 0) {
         analogWrite(VENT_PIN, 255);
         delay(200);
-        analogWrite(VENT_PIN, velocidadVentilador);
-      } else if (velocidadVentilador == 0) {
-        analogWrite(VENT_PIN, velocidadVentilador);
       }
+      analogWrite(VENT_PIN, potenciaVentilador);
     }
   }
 }
